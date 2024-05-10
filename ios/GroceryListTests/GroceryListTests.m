7d8b7c0ae93908106c7879de9b620fe445eb2f4e
timoxin <timoxin@MacBook-Pro---Artur.local>
2024-05-10 23:40:03 +0300

    [Feat] rename project

--- ios/GroceryListTests/GroceryListTests.m
+++ ios/GroceryListTests/GroceryListTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactNativeAppTests : XCTestCase
+@interface GroceryListTests : XCTestCase
 
 @end
 
-@implementation reactNativeAppTests
+@implementation GroceryListTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
